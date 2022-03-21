d706f7720e2a847d44b993ab6fe1ee663978fbaf
Finn Vos <f.vos@dr-flex.de>
2022-03-21 10:35:47 +0100

    Add subtract function

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ fn add(a: i32, b: i32) -> i32 {
     a + b
 }
 
+fn subtract(a: i32, b: i32) -> i32 {
+    a - b
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -9,5 +13,10 @@ mod tests {
     #[test]
     fn test_add() {
         assert_eq!(add(4, 6), 10);
+    }
+
+    #[test]
+    fn test_subtract() {
+        assert_eq!(subtract(7, 3), 4);
     }
 }
